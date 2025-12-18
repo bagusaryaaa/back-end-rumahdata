7fb4ecdb4328cb3f0e0b7bb4d9db4f9f89cb1fdb
bagusarya._ <aryawirianda@gmail.com>
2025-12-18 09:39:11 +0800

    fix all

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,21 +1,9 @@
 import express from "express";
-import {
-  register,
-  login,
-  logout,
-  approveUser,
-} from "../controllers/authController.js";
+import { register, login, logout } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-
 router.post("/logout", logout);
-
-router.post(
-  "/approve",
-  approveUser
-);
-
 export default router;
