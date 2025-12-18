c67880f85e6d3966d75d703454479cb08d10b536
I Nyoman Bagus Arya Wirianda <114628063+bagusaryaaa@users.noreply.github.com>
2025-12-18 08:16:14 +0800

    Delete Middlewares directory

--- Middlewares/roleMiddleware.js
+++
@@ -1,8 +0,0 @@
-export const superadminOnly = (req, res, next) => {
-  if (req.user.role !== "superadmin") {
-    return res.status(403).json({
-      error: "Akses ditolak (Superadmin only)",
-    });
-  }
-  next();
-};
